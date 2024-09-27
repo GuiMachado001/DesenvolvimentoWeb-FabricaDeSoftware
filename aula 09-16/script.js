98da1dba757e4bffd0377cf66ddd5f12483e48d9
Guilherme Machado <gui.m;neves01@gmail.com>
2024-09-27 11:43:11 -0400

    Projeto Aula 27/09

---
+++ aula 09-16/script.js
@@ -0,0 +1,7 @@
+
+$(document).ready(function() {
+    $('.mobileBtn').on('click', function(){
+        $('.mobileMenu').toggleClass('active');
+        $('.mobileBtn').find('i').toggleClass('fa-x')
+    });
+});
