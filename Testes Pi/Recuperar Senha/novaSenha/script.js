8cbc14dde85c4563cb6f2e0fd536eb529e119a24
Guilherme Machado <gui.m;neves01@gmail.com>
2024-10-04 07:39:42 -0400

    Atualização projeto Fábrica de Software

--- Testes Pi/Recuperar Senha/novaSenha/script.js
+++ Testes Pi/Recuperar Senha/novaSenha/script.js
@@ -7,28 +7,24 @@ function validarSenha(novaSenha) {
 
 
     
-    // Checa o comprimento da senha
     if (novaSenha.length >= 8) {
         liDigito.classList.add('requisitosValidos');
     } else {
         liDigito.classList.add('requisitosInvalidos');
     }
 
-    // Checa se tem letra maiúscula
     if (/[A-Z]/.test(novaSenha)) {
         liMaiuscula.classList.add('requisitosValidos');
     } else {
         liMaiuscula.classList.add('requisitosInvalidos');
     }
 
-    // Checa se tem dígito
     if (/[0-9]/.test(novaSenha)) {
         liNumero.classList.add('requisitosValidos');
     } else {
         liNumero.classList.add('requisitosInvalidos');
     }
 
-    // Checa se tem caractere especial
     if (/[!@#$%^&*(),.?":{}|<>]/.test(novaSenha)) {
         liCaracEspec.classList.add('requisitosValidos');
     } else {
@@ -36,7 +32,6 @@ function validarSenha(novaSenha) {
     }
 }
 
-// Adiciona um evento de input ao campo de senha
 document.getElementById('novaSenha').addEventListener('input', function() {
     validarSenha(this.value);
 });
