d78027cf5d516a51c67f967dd49132c954148d50
Guilherme Machado Neves <gui.m.neves01@gmail.com>
2024-10-14 16:36:08 -0400

    Exercicios

--- Aula10-14 - js/exercicioHtml/script.js
+++ Aula10-14 - js/exercicioHtml/script.js
@@ -1,33 +1,13 @@
-// function abrir(botao)
+function abrir(botao) {
+    var respostaContainer = botao.parentElement.querySelector('.respostaContainer');
+    
+    var visivel = respostaContainer.style.display !== "none";
 
-// {
-//   var td = botao.parentElement;
-//   var spans = td.getElementsByTagName('span');
-//   var verMaisVisivel = spans[1].style.display == "none";
-
-//   if (verMaisVisivel) {
-//     spans[0].style.display = spans[0].style.display = "none";
-//     spans[1].style.display = spans[1].style.display = "block";
-//     botao.innerHTML = "Ver Menos";
-//   } else {
-//     spans[0].style.display = spans[0].style.display = "block";
-//     spans[1].style.display = spans[1].style.display = "none";
-//     botao.innerHTML = "Ver Mais";
-//   }
-// }
-
-function abrir(botao){
-    var td = botao.parentElement;
-    var teste = td.document.querySelectorAll('.respostaContainer');
-    // var verMaisVisivel = teste.style.display == "none";
-
-    if(td){
-        teste.remove('displayNone');
-        teste.add('displayBlock')
-        botao.innerHTML = "Ver Menos";
-    }else{
-        teste.remove('displayBlock');
-        teste.add('displayNone')
+    if (!visivel) {
+        respostaContainer.style.display = "block"; 
+        botao.innerHTML = "Ver Menos"; 
+    } else {
+        respostaContainer.style.display = "none";
         botao.innerHTML = "Ver Mais";
     }
 }
